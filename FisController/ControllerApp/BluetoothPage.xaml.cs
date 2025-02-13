e7cf8a258a5ad801c50a5ab84595e164a3e1814f
Artis Davidenis <artis.davidenis@gmail.com>
2025-02-13 22:55:35 +0200

    #maui-bluetooth added basic ble functionality

--- FisController/ControllerApp/BluetoothPage.xaml.cs
+++ FisController/ControllerApp/BluetoothPage.xaml.cs
@@ -1,54 +1,71 @@
-using Android.Bluetooth;
-using Plugin.BLE;
+using ControllerApp.Services;
 using Plugin.BLE.Abstractions.Contracts;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace ControllerApp
 {
 	public partial class BluetoothPage : ContentPage
 	{
-        private IBluetoothLE ble;
-        private IAdapter adapter;
         public ObservableCollection<IDevice> Devices { private set; get; }
+        public ICommand ConnectCommand { get; private set; }
+        private BleService bleService;
 
         public BluetoothPage()
 		{
 			InitializeComponent();
+            bleService = new BleService();
             Initialize();
             BindingContext = this;
+
+            ConnectCommand = new Command<IDevice>(async (device) => await ConnectToDevice(device));
         }
 
         public void Initialize()
         {
-            ble = CrossBluetoothLE.Current;
-            adapter = CrossBluetoothLE.Current.Adapter;
+            BleStatusLabel.Dispatcher.Dispatch(() =>
+            {
+                BleStatusLabel.Text = bleService.GetBleStatus();
+            });
+            //adapter = bleService.StartScan().Result;
             Devices = new ObservableCollection<IDevice>();
 
-            BleStatusLabel.Text = ble.State.ToString();
-
-            ble.StateChanged += (s, e) =>
-            {
-                BleStatusLabel.Dispatcher.Dispatch(() =>
-                {
-                    BleStatusLabel.Text = ble.State.ToString();
-                });
-            };
-
-            adapter.DeviceDiscovered += (s, a) =>
-            {
-                MainThread.BeginInvokeOnMainThread(() =>
-                {
-                    Devices.Add(a.Device);
-                });
-            };
-
-
-            DeviceList.ItemsSource = Devices;
+            bleService.SetupDevicesDiscoveredEvent(Devices);
+            //DeviceList.ItemsSource = Devices;
         }
 
         private async void OnScanForDevices_Clicked(object sender, EventArgs e)
         {
-            await adapter.StartScanningForDevicesAsync();
+            await bleService.StartScan();
+        }
+
+        private async void OnTest_Clicked(object sender, EventArgs e)
+        {
+            await bleService.TestConnection();
+        }
+
+        private async Task ConnectToDevice(IDevice device)
+        {
+            var result = await bleService.TryConnectToDevice(device);
+            SetBleConnectStatusLabelDispatch(result);
+        }
+
+        private void SetBleConnectStatusLabelDispatch(bool connectResult)
+        {
+            if (connectResult)
+            {
+                BleConnectStatusLabel.Dispatcher.Dispatch(() =>
+                {
+                    BleConnectStatusLabel.Text = "Connected";
+                });
+            }
+            else
+            {
+                BleConnectStatusLabel.Dispatcher.Dispatch(() =>
+                {
+                    BleConnectStatusLabel.Text = "Failed to connect";
+                });
+            }
         }
 
         private async void SetError(Exception ex)
